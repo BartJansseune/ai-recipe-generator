a9d95f90eb93af19885f469bfe63c710fdfafcbf
Bart Jansseune <bart.jansseune@midiagnostics.com>
2026-02-24 13:11:16 +0100

    Add Amplify hosting build configuration

---
+++ src/amplify_outputs.d.ts
@@ -0,0 +1,6 @@
+// Type declaration for amplify_outputs.json
+// This allows TypeScript to compile even if the file doesn't exist yet
+declare module '../amplify_outputs.json' {
+  const value: any;
+  export default value;
+}
